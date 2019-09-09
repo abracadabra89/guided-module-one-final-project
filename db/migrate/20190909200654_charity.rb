53586e016abb6273e354ac9674a1e14c5f33365c
Anya <anya.s.litvinova@gmail.com>
2019-09-09 16:45:15 -0400

    Done.

---
+++ db/migrate/20190909200654_charity.rb
@@ -0,0 +1,9 @@
+class Charity < ActiveRecord::Migration[5.0]
+  def change
+      create_table :charities  do |t|
+          t.string :name
+          t.string :location
+          t.integer :founding_year
+      end
+  end
+end
